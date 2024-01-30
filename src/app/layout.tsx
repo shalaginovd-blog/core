90ac018913f583cdd3f3d4c0df28a806edb7d842
Даня <45875910+danyatochkaru@users.noreply.github.com>
2024-01-30 18:52:14 +0300

    chor: prettier и shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FornSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/shared/ui/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FornSans({
+  subsets: ["cyrillic", "latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru">
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+      )}>{children}</body>
     </html>
   );
 }
